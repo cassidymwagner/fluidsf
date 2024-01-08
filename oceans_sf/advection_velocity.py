2feb26ce75d405dfbe435b381c37de3c5140e528
Cassidy Wagner <cmwagner@protonmail.com>
2024-01-07 19:19:58 -0800

    wip testing sf calc with new refactor

--- oceans_sf/advection_velocity.py
+++ oceans_sf/advection_velocity.py
@@ -22,15 +22,39 @@ def advection_velocity(
     grid_type="uniform",
     nbins=10,
 ):
-    """Add docstring."""
+    """
+    Calculate the advection velocity structure function for 2D u and v velocity fields.
+
+    Args:
+    ----
+        u (ndarray): 2D array of u velocity components.
+        v (ndarray): 2D array of v velocity components.
+        x (ndarray): 2D array of x-coordinates.
+        y (ndarray): 1D array of y-coordinates.
+        dx (float, optional): Grid spacing in the x-direction. Defaults to None.
+        dy (float, optional): Grid spacing in the y-direction. Defaults to None.
+        boundary (str, optional): Boundary condition of the data.
+        Defaults to "Periodic".
+        even (bool, optional): Flag indicating if the grid is evenly spaced.
+        Defaults to True.
+        grid_type (str, optional): Type of grid, either "uniform" or "latlon".
+        Defaults to "uniform".
+        nbins (int, optional): Number of bins for binning the data. Defaults to 10.
+
+    Returns:
+    -------
+        dict: Dictionary containing the advection velocity structure functions
+        and separation distances for the x- and y-direction (zonal and meridional).
+
+    """
     # Define a list of separation distances to iterate over.
     # Periodic is half the length since the calculation will wrap the data.
     if boundary == "Periodic":
-        sep_z = range(int(len(x) / 2))
-        sep_m = range(int(len(y) / 2))
+        sep_z = range(1, int(len(x) / 2))
+        sep_m = range(1, int(len(y) / 2))
     else:
-        sep_z = range(int(len(x) - 1))
-        sep_m = range(int(len(y) - 1))
+        sep_z = range(1, int(len(x) - 1))
+        sep_m = range(1, int(len(y) - 1))
 
     # Initialize the separation distance arrays
     xd = np.zeros(np.shape(sep_z))
@@ -39,14 +63,14 @@ def advection_velocity(
     # Initialize the structure function arrays
     SF_z = np.zeros(np.shape(sep_z))
     SF_m = np.zeros(np.shape(sep_m))
-
+    print(sep_z, sep_m)
     adv_E, adv_N = calculate_velocity_advection(u, v, x, y, dx, dy, grid_type)
-
+    # print(adv_E, adv_N)
     # Iterate over separations left and down
-    for down, left in (sep_m, sep_z):
+    for down, left in zip(sep_m, sep_z):
         xroll = shift_array1d(x, shift_by=left, boundary=boundary)
         yroll = shift_array1d(y, shift_by=down, boundary=boundary)
-
+        print(xroll, yroll)
         adv_E_roll_left, adv_E_roll_down = shift_array2d(
             adv_E, shift_down=down, shift_left=left, boundary=boundary
         )
@@ -94,6 +118,8 @@ def advection_velocity(
     if even is False:
         xd, SF_z = bin_data(xd, SF_z)
         yd, SF_m = bin_data(yd, SF_m)
+
+    print(xd, yd, SF_z, SF_m)
 
     data = {
         "SF_zonal": SF_z,
--- tests/test_advection_velocity.py
+++ tests/test_advection_velocity.py
@@ -10,13 +10,32 @@ from oceans_sf.advection_velocity import advection_velocity
     "u, v, x, y, dx, dy, boundary, even, grid_type, nbins, expected_result",
     [
         # Test case 1
+        # (
+        #     np.array([[1, 2], [3, 4]]),
+        #     np.array([[1, -2], [-3, 4]]),
+        #     np.linspace(0, 1, 2),
+        #     np.linspace(0, 1, 2),
+        #     None,
+        #     None,
+        #     "Periodic",
+        #     True,
+        #     "uniform",
+        #     10,
+        #     {
+        #         "SF_zonal": np.array([0.0, 0.0]),
+        #         "SF_meridional": np.array([0.0, 0.0]),
+        #         "x-diffs": np.array([0.0, 1.0]),
+        #         "y-diffs": np.array([0.0, 1.0]),
+        #     },
+        # ),
+        # Test case 2
         (
-            np.array([[1, 2, 3], [4, 5, 6]]),
-            np.array([[7, 8, 9], [10, 11, 12]]),
-            np.array([0, 1, 2]),
-            np.array([0, 1, 2]),
-            1.0,
-            1.0,
+            np.array([[1, 2, 3, 4, 5], [6, 7, 8, 9, 10]]),
+            np.array([[11, 12, 13, 14, 15], [16, 17, 18, 19, 20]]),
+            np.array([0, 1, 2, 3, 4]),
+            np.array([0, 1, 2, 3, 4]),
+            None,
+            None,
             "Periodic",
             True,
             "uniform",
@@ -28,25 +47,6 @@ from oceans_sf.advection_velocity import advection_velocity
                 "y-diffs": np.array([0.0, 1.0]),
             },
         ),
-        # # Test case 2
-        # (
-        #     np.array([[1, 2, 3], [4, 5, 6]]),
-        #     np.array([[7, 8, 9], [10, 11, 12]]),
-        #     np.array([0, 1, 2]),
-        #     np.array([0, 1, 2]),
-        #     1.0,
-        #     1.0,
-        #     "Non-Periodic",
-        #     False,
-        #     "uniform",
-        #     10,
-        #     {
-        #         "SF_zonal": np.array([0.0, 0.0]),
-        #         "SF_meridional": np.array([0.0, 0.0]),
-        #         "x-diffs": np.array([0.0, 1.0]),
-        #         "y-diffs": np.array([0.0, 1.0]),
-        #     },
-        # ),
         # Add more test cases here...
     ],
 )
@@ -54,4 +54,9 @@ def test_advection_velocity(
     u, v, x, y, dx, dy, boundary, even, grid_type, nbins, expected_result
 ):
     result = advection_velocity(u, v, x, y, dx, dy, boundary, even, grid_type, nbins)
-    TestCase().assertDictEqual(expected_result, result)
+    # np.testing.assert_allclose(result["SF_zonal"], expected_result["SF_zonal"])
+    # np.testing.assert_allclose(
+    #     result["SF_meridional"], expected_result["SF_meridional"]
+    # )
+    np.testing.assert_allclose(result["x-diffs"], expected_result["x-diffs"])
+    # np.testing.assert_allclose(result["y-diffs"], expected_result["y-diffs"])
