460128182f3085222fd3d67d229b24f543b6dcbc
Cassidy Wagner <cmwagner@protonmail.com>
2024-01-07 16:28:52 -0800

    changed dx,dy order to be consistent

--- oceans_sf/calculate_velocity_advection.py
+++ oceans_sf/calculate_velocity_advection.py
@@ -32,15 +32,15 @@ def calculate_velocity_advection(
         xcoords = dx.cumsum()
         ycoords = dy.cumsum()
 
-        dudx, dudy = np.gradient(u, xcoords, ycoords, axis=(0, 1))
-        dvdx, dvdy = np.gradient(v, xcoords, ycoords, axis=(0, 1))
+        dudy, dudx = np.gradient(u, xcoords, ycoords, axis=(0, 1))
+        dvdy, dvdx = np.gradient(v, xcoords, ycoords, axis=(0, 1))
 
     else:
         dx = np.abs(x[0] - x[1])
         dy = np.abs(y[0] - y[1])
 
-        dudx, dudy = np.gradient(u, dx, dy, axis=(0, 1))
-        dvdx, dvdy = np.gradient(v, dx, dy, axis=(0, 1))
+        dudy, dudx = np.gradient(u, dx, dy, axis=(0, 1))
+        dvdy, dvdx = np.gradient(v, dx, dy, axis=(0, 1))
 
     eastward_advection = u * dudx + v * dudy
     northward_advection = u * dvdx + v * dvdy
