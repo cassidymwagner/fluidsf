face2163fa1052c8385c6500e13552c2776041be
Cassidy Wagner <cmwagner@protonmail.com>
2024-01-07 17:49:20 -0800

    added functions

--- oceans_sf/__init__.py
+++ oceans_sf/__init__.py
@@ -5,7 +5,12 @@ This package calculates structure functions from ocean velocity data.
 
 from .advection_scalar import advection_scalar
 from .advection_velocity import advection_velocity
+from .bin_data import bin_data
+from .calculate_advection_velocity_structure_function import (
+    calculate_advection_velocity_structure_function,
+)
 from .calculate_scalar_advection import calculate_scalar_advection
+from .calculate_separation_distances import calculate_separation_distances
 from .calculate_velocity_advection import calculate_velocity_advection
 from .shift_array1d import shift_array1d
 from .shift_array2d import shift_array2d
@@ -15,10 +20,13 @@ from .traditional_velocity import traditional_velocity
 __all__ = (
     "advection_scalar",
     "advection_velocity",
+    "calculate_advection_velocity_structure_function",
     "calculate_scalar_advection",
     "calculate_velocity_advection",
+    "calculate_separation_distances",
     "shift_array1d",
     "shift_array2d",
+    "bin_data",
     "traditional_velocity",
     "traditional_scalar",
 )
