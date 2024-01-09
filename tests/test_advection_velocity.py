f5b2a2a5865618c3665cea2688e0cee5c89abf2b
Cassidy Wagner <cmwagner@protonmail.com>
2024-01-08 16:59:16 -0800

    added nonperiodic test

--- tests/test_advection_velocity.py
+++ tests/test_advection_velocity.py
@@ -26,6 +26,27 @@ from oceans_sf.advection_velocity import advection_velocity
                 "y-diffs": np.array([0, 1, 2, 3, 4]),
             },
         ),
+        (
+            np.array([[i + 1 for i in range(j * 10, (j + 1) * 10)] for j in range(10)]),
+            np.array([[i + 1 for i in range(j * 10, (j + 1) * 10)] for j in range(10)])
+            * 2,
+            np.linspace(1, 10, 10),
+            np.linspace(1, 10, 10),
+            None,
+            None,
+            None,
+            True,
+            "uniform",
+            10,
+            {
+                "SF_zonal": np.array([0, 105, 420, 945, 1680, 2625, 3780, 5145, 6720]),
+                "SF_meridional": np.array(
+                    [0, 10500, 42000, 94500, 168000, 262500, 378000, 514500, 672000]
+                ),
+                "x-diffs": np.array([0, 1, 2, 3, 4, 5, 6, 7, 8]),
+                "y-diffs": np.array([0, 1, 2, 3, 4, 5, 6, 7, 8]),
+            },
+        ),
     ],
 )
 def test_advection_velocity(
