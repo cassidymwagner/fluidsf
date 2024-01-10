466864ca18b98f3c1644a6d7f1717f511ac97c14
Cassidy Wagner <cmwagner@protonmail.com>
2024-01-09 17:08:11 -0800

    added docstring and fixed np.gradient bug

--- oceans_sf/calculate_scalar_advection.py
+++ oceans_sf/calculate_scalar_advection.py
@@ -2,24 +2,40 @@ import numpy as np
 
 
 def calculate_scalar_advection(
-    scalar, par_u, par_v, x, y, dx=None, dy=None, grid_type="uniform"
+    u, v, x, y, scalar, dx=None, dy=None, grid_type="uniform"
 ):
-    """Add docstring."""
-    u = par_u
-    v = par_v
-    s = scalar
+    """
+    Calculate the scalar advection field using the given velocity components and
+    scalar field.
 
+    Args:
+    ----
+        u (ndarray): The u-component of velocity.
+        v (ndarray): The v-component of velocity.
+        x (ndarray): The x-coordinates of the grid.
+        y (ndarray): The y-coordinates of the grid.
+        scalar (ndarray): The scalar field.
+        dx (float or ndarray, optional): The grid spacing in the x-direction.
+        Defaults to None.
+        dy (float or ndarray, optional): The grid spacing in the y-direction.
+        Defaults to None.
+        grid_type (str, optional): The type of grid. Defaults to "uniform".
+
+    Returns:
+    -------
+        ndarray: The scalar advection field.
+    """
     if grid_type == "latlon":
         xcoords = dx.cumsum()
         ycoords = dy.cumsum()
 
-        dsdx, dsdy = np.gradient(s, xcoords, ycoords, axis=(0, 1))
+        dsdy, dsdx = np.gradient(scalar, xcoords, ycoords, axis=(0, 1))
 
     else:
         dx = np.abs(x[0] - x[1])
         dy = np.abs(y[0] - y[1])
 
-        dsdx, dsdy = np.gradient(s, dx, dy, axis=(0, 1))
+        dsdy, dsdx = np.gradient(scalar, dx, dy, axis=(0, 1))
 
     advection = u * dsdx + v * dsdy
 
