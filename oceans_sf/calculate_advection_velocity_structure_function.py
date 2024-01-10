084569699b41ba3a316a05ed8ffb8dd86d56121a
Cassidy Wagner <cmwagner@protonmail.com>
2024-01-09 16:15:27 -0800

    docstring updates

--- oceans_sf/calculate_advection_velocity_structure_function.py
+++ oceans_sf/calculate_advection_velocity_structure_function.py
@@ -5,18 +5,18 @@ def calculate_advection_velocity_structure_function(
     u, v, adv_e, adv_n, u_shift, v_shift, adv_e_shift, adv_n_shift
 ):
     """
-    Calculate the advection velocity structure function.
+    Calculate the velocity-based advective structure function.
 
     Args
     ----
     u (numpy.ndarray): Array of u velocities.
     v (numpy.ndarray): Array of v velocities.
-    adv_e (numpy.ndarray): Array of eastward advection velocities.
-    adv_n (numpy.ndarray): Array of northward advection velocities.
+    adv_e (numpy.ndarray): Array of eastward advection values.
+    adv_n (numpy.ndarray): Array of northward advection values.
     u_shift (numpy.ndarray): Array of shifted u velocities.
     v_shift (numpy.ndarray): Array of shifted v velocities.
-    adv_e_shift (numpy.ndarray): Array of shifted eastward advection velocities.
-    adv_n_shift (numpy.ndarray): Array of shifted northward advection velocities.
+    adv_e_shift (numpy.ndarray): Array of shifted eastward advection values.
+    adv_n_shift (numpy.ndarray): Array of shifted northward advection values.
 
     Returns
     -------
