31f63d881c2aeba1fcb6e081a0eec81bac602d97
Cassidy Wagner <cmwagner@protonmail.com>
2024-01-11 15:16:50 -0800

    added inline comments for inputs

--- tests/test_calculate_advection.py
+++ tests/test_calculate_advection.py
@@ -9,44 +9,44 @@ from oceans_sf.calculate_advection import calculate_advection
     [
         # Test case for velocity field with zero values
         (
-            np.array([[0] * 3 for i in range(3)]),
-            np.array([[0] * 3 for i in range(3)]),
-            np.linspace(0, 3, 4),
-            np.linspace(0, 3, 4),
-            None,
-            None,
-            "uniform",
-            None,
+            np.array([[0] * 3 for i in range(3)]),  # u
+            np.array([[0] * 3 for i in range(3)]),  # v
+            np.linspace(0, 3, 4),  # x
+            np.linspace(0, 3, 4),  # y
+            None,  # dx
+            None,  # dy
+            "uniform",  # grid_type
+            None,  # scalar
             (
-                np.array([[0] * 3 for i in range(3)]),
-                np.array([[0] * 3 for i in range(3)]),
+                np.array([[0] * 3 for i in range(3)]),  # expected u_advection
+                np.array([[0] * 3 for i in range(3)]),  # expected v_advection
             ),
         ),
         # Test case for velocity field with non-zero values
         (
-            np.array([[1] * 3 for i in range(3)]),
-            np.array([[1] * 3 for i in range(3)]),
-            np.linspace(0, 3, 4),
-            np.linspace(0, 3, 4),
-            None,
-            None,
-            "uniform",
-            None,
+            np.array([[1] * 3 for i in range(3)]),  # u
+            np.array([[1] * 3 for i in range(3)]),  # v
+            np.linspace(0, 3, 4),  # x
+            np.linspace(0, 3, 4),  # y
+            None,  # dx
+            None,  # dy
+            "uniform",  # grid_type
+            None,  # scalar
             (
-                np.array([[0] * 3 for i in range(3)]),
-                np.array([[0] * 3 for i in range(3)]),
+                np.array([[0] * 3 for i in range(3)]),  # expected u_advection
+                np.array([[0] * 3 for i in range(3)]),  # expected v_advection
             ),
         ),
         # Test case for velocity field with non-zero values and non-uniform grid
         (
-            np.array([[1, 2, 3], [4, 5, 6], [7, 8, 9]]),
-            np.array([[1, 2, 3], [4, 5, 6], [7, 8, 9]]),
-            np.linspace(0, 2, 3),
-            np.linspace(0, 2, 3),
-            None,
-            None,
-            "uniform",
-            None,
+            np.array([[1, 2, 3], [4, 5, 6], [7, 8, 9]]),  # u
+            np.array([[1, 2, 3], [4, 5, 6], [7, 8, 9]]),  # v
+            np.linspace(0, 2, 3),  # x
+            np.linspace(0, 2, 3),  # y
+            None,  # dx
+            None,  # dy
+            "uniform",  # grid_type
+            None,  # scalar
             (
                 np.array(
                     [
@@ -54,110 +54,110 @@ from oceans_sf.calculate_advection import calculate_advection
                         [2 * 8, 2 * 10, 2 * 12],
                         [2 * 14, 2 * 16, 2 * 18],
                     ]
-                ),
+                ),  # expected u_advection
                 np.array(
                     [
                         [2 * 2, 2 * 4, 2 * 6],
                         [2 * 8, 2 * 10, 2 * 12],
                         [2 * 14, 2 * 16, 2 * 18],
                     ]
-                ),
+                ),  # expected v_advection
             ),
         ),
         # Test case for velocity field with positive and negative values
         (
-            np.array([[1, 2], [3, 4]]),
-            np.array([[1, -2], [-3, 4]]),
-            np.linspace(0, 1, 2),
-            np.linspace(0, 1, 2),
-            None,
-            None,
-            "uniform",
-            None,
+            np.array([[1, 2], [3, 4]]),  # u
+            np.array([[1, -2], [-3, 4]]),  # v
+            np.linspace(0, 1, 2),  # x
+            np.linspace(0, 1, 2),  # y
+            None,  # dx
+            None,  # dy
+            "uniform",  # grid_type
+            None,  # scalar
             (
                 np.array(
                     [
                         [3, -2],
                         [-3, 12],
                     ]
-                ),
+                ),  # expected u_advection
                 np.array(
                     [
                         [-7, -18],
                         [33, 52],
                     ]
-                ),
+                ),  # expected v_advection
             ),
         ),
         # Test case for scalar field with non-zero values and latlon grid
         (
-            np.array([[1, 2, 3], [4, 5, 6], [7, 8, 9]]),
-            np.array([[1, 2, 3], [4, 5, 6], [7, 8, 9]]),
-            np.array([0, 1, 1]),
-            np.array([0, 1, 1]),
-            np.array([0, 1, 1]),
-            np.array([0, 1, 1]),
-            "latlon",
-            np.array([[1, 2, 3], [4, 5, 6], [7, 8, 9]]),
+            np.array([[1, 2, 3], [4, 5, 6], [7, 8, 9]]),  # u
+            np.array([[1, 2, 3], [4, 5, 6], [7, 8, 9]]),  # v
+            np.array([0, 1, 1]),  # x
+            np.array([0, 1, 1]),  # y
+            np.array([0, 1, 1]),  # dx
+            np.array([0, 1, 1]),  # dy
+            "latlon",  # grid_type
+            np.array([[1, 2, 3], [4, 5, 6], [7, 8, 9]]),  # scalar
             np.array(
                 [
                     [2 * 2, 2 * 4, 2 * 6],
                     [2 * 8, 2 * 10, 2 * 12],
                     [2 * 14, 2 * 16, 2 * 18],
                 ]
-            ),
+            ),  # expected scalar advection
         ),
         # Test case for scalar field with positive and negative values and latlon grid
         (
-            np.array([[1, 2], [3, 4]]),
-            np.array([[1, -2], [-3, 4]]),
-            np.array([0, 1]),
-            np.array([0, 1]),
-            np.array([0, 1]),
-            np.array([0, 1]),
-            "latlon",
-            np.array([[1, 2], [3, 4]]),
+            np.array([[1, 2], [3, 4]]),  # u
+            np.array([[1, -2], [-3, 4]]),  # v
+            np.array([0, 1]),  # x
+            np.array([0, 1]),  # y
+            np.array([0, 1]),  # dx
+            np.array([0, 1]),  # dy
+            "latlon",  # grid_type
+            np.array([[1, 2], [3, 4]]),  # scalar
             np.array(
                 [
                     [3, -2],
                     [-3, 12],
                 ]
-            ),
+            ),  # expected scalar advection
         ),
         # Test case for scalar field with non-zero values and uniform grid
         (
-            np.array([[1, 2, 3], [4, 5, 6], [7, 8, 9]]),
-            np.array([[1, 2, 3], [4, 5, 6], [7, 8, 9]]),
-            np.array([0, 1, 1]),
-            np.array([0, 1, 1]),
-            np.array([0, 1, 1]),
-            np.array([0, 1, 1]),
-            "uniform",
-            np.array([[1, 2, 3], [4, 5, 6], [7, 8, 9]]),
+            np.array([[1, 2, 3], [4, 5, 6], [7, 8, 9]]),  # u
+            np.array([[1, 2, 3], [4, 5, 6], [7, 8, 9]]),  # v
+            np.array([0, 1, 1]),  # x
+            np.array([0, 1, 1]),  # y
+            np.array([0, 1, 1]),  # dx
+            np.array([0, 1, 1]),  # dy
+            "uniform",  # grid_type
+            np.array([[1, 2, 3], [4, 5, 6], [7, 8, 9]]),  # scalar
             np.array(
                 [
                     [2 * 2, 2 * 4, 2 * 6],
                     [2 * 8, 2 * 10, 2 * 12],
                     [2 * 14, 2 * 16, 2 * 18],
                 ]
-            ),
+            ),  # expected scalar advection
         ),
         # Test case for scalar field with positive and negative values and uniform grid
         (
-            np.array([[1, 2], [3, 4]]),
-            np.array([[1, -2], [-3, 4]]),
-            np.array([0, 1]),
-            np.array([0, 1]),
-            np.array([0, 1]),
-            np.array([0, 1]),
-            "uniform",
-            np.array([[1, 2], [3, 4]]),
+            np.array([[1, 2], [3, 4]]),  # u
+            np.array([[1, -2], [-3, 4]]),  # v
+            np.array([0, 1]),  # x
+            np.array([0, 1]),  # y
+            np.array([0, 1]),  # dx
+            np.array([0, 1]),  # dy
+            "uniform",  # grid_type
+            np.array([[1, 2], [3, 4]]),  # scalar
             np.array(
                 [
                     [3, -2],
                     [-3, 12],
                 ]
-            ),
+            ),  # expected scalar advection
         ),
     ],
 )
