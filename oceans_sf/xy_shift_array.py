3016d0c672d623c8cd796d653f69f3297fb45342
Brodie Pearson <brodie.pearson@oregonstate.edu>
2024-05-09 22:24:55 -0700

    Update xy_shift_array.py

--- oceans_sf/xy_shift_array.py
+++ oceans_sf/xy_shift_array.py
@@ -1,12 +1,12 @@
 import numpy as np
 
 
-def shift_array2d(  # noqa: D417
+def xy_shift_array(  # noqa: D417
     input_array, shift_right=0, shift_down=0, boundary="periodic-all"
 ):
     """
-    Shifts 2D array right and down by the specified integer amounts and returns
-    the shifted arrays. Either wraps the array or shifts and pads with NaNs.
+    Wraps 2D array right and down by the specified integer amounts and returns
+    the shifted arrays. Only works with 2D, doubly-periodic data on an even grid.
 
     Parameters
     ----------
@@ -29,15 +29,15 @@ def shift_array2d(  # noqa: D417
     Returns
     -------
         xy_shifted_array
-            2D array shifted i the x-y directions by the specified integer amount
+            2D array shifted in the x-y directions by the specified integer amount
     """
     xy_shifted_array = np.full(np.shape(input_array), np.nan)
 
     if boundary == "periodic-all":
-        xy_shifted_array[:, :shift_right] = input_array[:, -shift_right:]
-        xy_shifted_array[:, shift_right:] = input_array[:, :-shift_right]
 
-        xy_shifted_array[:shift_down, :] = xy_shifted_array[-shift_down:, :]
-        xy_shifted_array[shift_down:, :] = xy_shifted_array[:-shift_down, :]
+        xy_shifted_array[:shift_down, :shift_right] = input_array[-shift_down:, -shift_right:]
+        xy_shifted_array[:shift_down, shift_right:] = input_array[-shift_down:, :-shift_right]
+        xy_shifted_array[shift_down:, :shift_right] = input_array[:-shift_down, -shift_right:]
+        xy_shifted_array[shift_down:, shift_right:] = input_array[:-shift_down, :-shift_right]
 
     return xy_shifted_array
