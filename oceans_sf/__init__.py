770064b8c4116ce5dad162484adb5631b2df036e
Cassidy Wagner <cmwagner@protonmail.com>
2024-05-13 15:29:22 -0700

    added new functions

--- oceans_sf/__init__.py
+++ oceans_sf/__init__.py
@@ -6,16 +6,18 @@ This package calculates structure functions from ocean velocity data.
 from .bin_data import bin_data
 from .calculate_advection import calculate_advection
 from .calculate_separation_distances import calculate_separation_distances
-from .calculate_structure_function import (
-    calculate_structure_function,
-)
+from .calculate_structure_function import calculate_structure_function
+from .calculate_structure_function_1d import calculate_structure_function_1d
 from .generate_structure_functions import generate_structure_functions
+from .generate_structure_functions_1d import generate_structure_functions_1d
 from .shift_array1d import shift_array1d
 from .shift_array2d import shift_array2d
 
 __all__ = (
     "generate_structure_functions",
+    "generate_structure_functions_1d",
     "calculate_structure_function",
+    "calculate_structure_function_1d",
     "calculate_advection",
     "calculate_separation_distances",
     "shift_array1d",
