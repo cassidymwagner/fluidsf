9c3340346da47588072caf5b23e716990f809f6f
Cassidy Wagner <cmwagner@protonmail.com>
2024-05-14 14:19:04 -0700

    uneven grid test

--- tests/test_generate_structure_functions_1d.py
+++ tests/test_generate_structure_functions_1d.py
@@ -8,7 +8,7 @@ from oceans_sf.generate_structure_functions_1d import generate_structure_functio
     "u, v, x, y, scalar, traditional_type, dx, boundary, even, grid_type, "
     "nbins, expected_dict",
     [
-        # Test 1: all traditional structure functions
+        # Test 1: all traditional structure functions for a uniform even grid
         (
             np.array([1, 2, 3, 4]),  # u
             np.array([2, 4, 6, 8]),  # v
@@ -16,11 +16,32 @@ from oceans_sf.generate_structure_functions_1d import generate_structure_functio
             None,  # y
             np.array([3, 6, 9, 12]),  # scalar
             ["LL", "LLL", "LTT", "LSS"],  # traditional_type
-            1,  # dx
+            None,  # dx
             None,  # boundary
             True,  # even
             "uniform",  # grid_type
             None,  # nbins
+            {
+                "SF_LL": np.array([0, 1, 4]),
+                "SF_LLL": np.array([0, -1, -8]),
+                "SF_LTT": np.array([0, -4, -32]),
+                "SF_LSS": np.array([0, -9, -72]),
+                "x-diffs": np.array([0, 1, 2]),
+            },
+        ),
+        # Test 2: all traditional structure functions for a uniform uneven grid
+        (
+            np.array([1, 2, 3, 4]),  # u
+            np.array([2, 4, 6, 8]),  # v
+            np.array([1, 2, 3, 4]),  # x
+            None,  # y
+            np.array([3, 6, 9, 12]),  # scalar
+            ["LL", "LLL", "LTT", "LSS"],  # traditional_type
+            None, # dx
+            None,  # boundary
+            False,  # even
+            "uniform",  # grid_type
+            3,  # nbins
             {
                 "SF_LL": np.array([0, 1, 4]),
                 "SF_LLL": np.array([0, -1, -8]),
