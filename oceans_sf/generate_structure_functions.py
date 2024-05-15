bc3919482c6a69fa1549260f8e2af4d3af031350
Cassidy Wagner <cmwagner@protonmail.com>
2024-05-15 15:11:25 -0700

    removed unecessary text

--- oceans_sf/generate_structure_functions.py
+++ oceans_sf/generate_structure_functions.py
@@ -24,7 +24,6 @@ def generate_structure_functions(  # noqa: C901, D417
     nbins=None,
 ):
     """
-    TEST
     Full method for generating structure functions for 2D data, either advective or
     traditional structure functions. Supports velocity-based and scalar-based structure
     functions. Defaults to calculating the velocity-based advective structure functions
