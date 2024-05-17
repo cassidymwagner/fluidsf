8dfc9d3d768a386da5bef6b6499f395f44c6ebdb
Cassidy Wagner <cmwagner@protonmail.com>
2024-05-17 13:39:29 -0700

    separation distances calculated for xyz for uniform grid

--- oceans_sf/calculate_separation_distances_3d.py
+++ oceans_sf/calculate_separation_distances_3d.py
@@ -1,8 +1,7 @@
 import numpy as np
-from geopy.distance import great_circle
 
 
-def calculate_separation_distances(x, y, x_shift, y_shift, grid_type="uniform"):
+def calculate_separation_distances_3d(x, y, z, x_shift, y_shift, z_shift):
     """
     Calculate the separation distances between two points.
 
@@ -12,26 +11,23 @@ def calculate_separation_distances(x, y, x_shift, y_shift, grid_type="uniform"):
         The x-coordinate of the first point.
     y: float
         The y-coordinate of the first point.
+    z: float
+        The z-coordinate of the first point.
     x_shift: float
         The x-coordinate of the second point.
     y_shift: float
         The y-coordinate of the second point.
-    grid_type: str
-        The type of grid used for the coordinates. A uniform grid results in a simple
-        distance calculation, but a latlon grid uses geopy's great_circle
-        function to estimate the distance in meters. Defaults to "uniform".
+    z_shift: float
+        The z-coordinate of the second point.
 
     Returns
     -------
     tuple:
-        A tuple containing the x and y separation distances. Either in meters in the
-        case of a latlon grid or code units for a uniform grid
+        A tuple containing the x, y, and z separation distances. In code units for a
+        uniform grid.
     """
-    if grid_type == "latlon":
-        xd = np.abs(great_circle((x_shift, y), (x, y)).meters)
-        yd = np.abs(great_circle((x, y_shift), (x, y)).meters)
-    else:
-        xd = np.abs(x_shift - x)
-        yd = np.abs(y_shift - y)
+    xd = np.abs(x_shift - x)
+    yd = np.abs(y_shift - y)
+    zd = np.abs(z_shift - z)
 
-    return (xd, yd)
+    return (xd, yd, zd)
