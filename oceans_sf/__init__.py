6a5c77d8bb46792f565cba4c2d54c4ad3e449f46
Cassidy Wagner <cmwagner@protonmail.com>
2024-05-17 13:28:12 -0700

    added all new functions

--- oceans_sf/__init__.py
+++ oceans_sf/__init__.py
@@ -7,20 +7,26 @@ from .bin_data import bin_data
 from .calculate_advection import calculate_advection
 from .calculate_advection_3d import calculate_advection_3d
 from .calculate_separation_distances import calculate_separation_distances
-from .calculate_structure_function import (
-    calculate_structure_function,
-)
+from .calculate_separation_distances_3d import calculate_separation_distances_3d
+from .calculate_structure_function import calculate_structure_function
+from .calculate_structure_function_3d import calculate_structure_function_3d
 from .generate_structure_functions import generate_structure_functions
+from .generate_structure_functions_3d import generate_structure_functions_3d
 from .shift_array1d import shift_array1d
 from .shift_array2d import shift_array2d
+from .shift_array3d import shift_array3d
 
 __all__ = (
     "generate_structure_functions",
+    "generate_structure_functions_3d",
     "calculate_structure_function",
+    "calculate_structure_function_3d",
     "calculate_advection",
     "calculate_advection_3d",
     "calculate_separation_distances",
+    "calculate_separation_distances_3d",
     "shift_array1d",
     "shift_array2d",
+    "shift_array3d",
     "bin_data",
 )
