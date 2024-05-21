398baa80c6eec2257c86fa9d821b450e099bd0db
Cassidy Wagner <cmwagner@protonmail.com>
2024-05-21 12:56:07 -0700

    formatting

--- oceans_sf/calculate_structure_function_3d.py
+++ oceans_sf/calculate_structure_function_3d.py
@@ -164,7 +164,7 @@ def calculate_structure_function_3d(  # noqa: D417, C901
                             (inputs["v_" + direction + "_shift"] - v)
                             * (inputs["u_" + direction + "_shift"] - u) ** 2
                         )
-                    
+
                     if direction == "z":
                         SF_dict["SF_LTT_" + direction] = None
 
