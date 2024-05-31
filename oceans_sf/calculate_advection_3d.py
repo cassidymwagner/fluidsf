f5277fe3c3384f96167c3a5ce8eb9c920c42602a
Brodie Pearson <brodie.pearson@oregonstate.edu>
2024-05-31 15:53:32 -0700

    Correct 3D advection operators

--- oceans_sf/calculate_advection_3d.py
+++ oceans_sf/calculate_advection_3d.py
@@ -45,32 +45,19 @@ def calculate_advection_3d(  # noqa: D417
     dz = np.abs(z[0] - z[1])
 
     if scalar is not None:
-        dsdz, dsdy, dsdx = np.gradient(scalar, dx, dy, dz, axis=(0, 1, 2))
+        dsdz, dsdy, dsdx = np.gradient(scalar, dz, dy, dx, axis=(0, 1, 2))
 
-        advection_i = v * dsdz - w * dsdy
-        advection_j = w * dsdx - u * dsdz
-        advection_k = u * dsdy - v * dsdx
-        advection = advection_i + advection_j + advection_k
+        advection = u * dsdx + v * dsdy + w * dsdz
 
     else:
-        dudz, dudy, dudx = np.gradient(u, dx, dy, dz, axis=(0, 1, 2))
-        dvdz, dvdy, dvdx = np.gradient(v, dx, dy, dz, axis=(0, 1, 2))
-        dwdz, dwdy, dwdx = np.gradient(w, dx, dy, dz, axis=(0, 1, 2))
+        dudz, dudy, dudx = np.gradient(u, dz, dy, dx, axis=(0, 1, 2))
+        dvdz, dvdy, dvdx = np.gradient(v, dz, dy, dx, axis=(0, 1, 2))
+        dwdz, dwdy, dwdx = np.gradient(w, dz, dy, dx, axis=(0, 1, 2))
 
-        east_adv_i = v * dudz - w * dudy
-        east_adv_j = w * dudx - u * dudz
-        east_adv_k = u * dudy - v * dudx
-        eastward_advection = east_adv_i + east_adv_j + east_adv_k
-
-        north_adv_i = v * dvdz - w * dvdy
-        north_adv_j = w * dvdx - u * dvdz
-        north_adv_k = u * dvdy - v * dvdx
-        northward_advection = north_adv_i + north_adv_j + north_adv_k
-
-        down_adv_i = v * dwdz - w * dwdy
-        down_adv_j = w * dwdx - u * dwdz
-        down_adv_k = u * dwdy - v * dwdx
-        downward_advection = down_adv_i + down_adv_j + down_adv_k
-        advection = (eastward_advection, northward_advection, downward_advection)
+        u_advection = u * dudx + v * dudy + w * dudz
+        v_advection = u * dvdx + v * dvdy + w * dvdz
+        w_advection = u * dwdx + v * dwdy + w * dwdz
+        
+        advection = (u_advection, v_advection, w_advection)
 
     return advection
