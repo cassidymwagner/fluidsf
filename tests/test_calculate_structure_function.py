f4dc6ebf163d8a4dff7ed0c8b6ab8193cfce2878
Cassidy Wagner <cmwagner@protonmail.com>
2024-07-12 16:42:48 -0700

    wip: analytical tests

--- tests/test_calculate_structure_function.py
+++ tests/test_calculate_structure_function.py
@@ -8,161 +8,287 @@ from fluidsf.calculate_structure_function import calculate_structure_function
     "u, v, adv_x, adv_y, shift_x, shift_y, skip_velocity_sf, scalar, "
     "adv_scalar, traditional_type, boundary, expected_result",
     [
-        # Test 1: velocity and scalar no traditional
+        # # Test 1: velocity and scalar no traditional
+        # (
+        #     np.array([[1, 2], [3, 4]]),  # u
+        #     np.array([[1, -2], [-3, 4]]),  # v
+        #     np.array([[3, -2], [-3, 12]]),  # adv_x
+        #     np.array([[-7, -18], [33, 52]]),  # adv_y
+        #     1,  # x_shift
+        #     1,  # y_shift
+        #     False,  # skip_velocity_sf
+        #     np.array([[1, 2], [3, 4]]),  # scalar
+        #     np.array([[3, -2], [-3, 12]]),  # adv_scalar
+        #     None,  # traditional_type
+        #     "periodic-all",  # boundary
+        #     {
+        #         "SF_velocity_x": 88,
+        #         "SF_velocity_y": 138,
+        #         "SF_scalar_x": 5,
+        #         "SF_scalar_y": 8,
+        #     },
+        #     # expected_result
+        # ),
+        # # Test 2: all
+        # (
+        #     np.array([[1, 2], [3, 4]]),
+        #     np.array([[1, -2], [-3, 4]]),
+        #     np.array([[3, -2], [-3, 12]]),
+        #     np.array([[-7, -18], [33, 52]]),
+        #     1,
+        #     1,
+        #     False,
+        #     np.array([[1, 2], [3, 4]]),
+        #     np.array([[3, -2], [-3, 12]]),
+        #     ["LL"],
+        #     "periodic-all",
+        #     {
+        #         "SF_velocity_x": 88,
+        #         "SF_LL_x": 1,
+        #         "SF_scalar_x": 5,
+        #         "SF_velocity_y": 138,
+        #         "SF_LL_y": 26,
+        #         "SF_scalar_y": 8,
+        #     },
+        # ),
+        # # Test 3: all but skip velocity and no traditional
+        # (
+        #     np.array([[1, 2], [3, 4]]),
+        #     np.array([[1, -2], [-3, 4]]),
+        #     np.array([[3, -2], [-3, 12]]),
+        #     np.array([[-7, -18], [33, 52]]),
+        #     1,
+        #     1,
+        #     True,
+        #     np.array([[1, 2], [3, 4]]),
+        #     np.array([[3, -2], [-3, 12]]),
+        #     None,
+        #     "periodic-all",
+        #     {
+        #         "SF_scalar_x": 5,
+        #         "SF_scalar_y": 8,
+        #     },
+        # ),
+        # # Test 4: velocities with no scalar or traditional
+        # (
+        #     np.array([[1, 2], [3, 4]]),
+        #     np.array([[1, -2], [-3, 4]]),
+        #     np.array([[3, -2], [-3, 12]]),
+        #     np.array([[-7, -18], [33, 52]]),
+        #     1,
+        #     1,
+        #     False,
+        #     None,
+        #     None,
+        #     None,
+        #     "periodic-all",
+        #     {
+        #         "SF_velocity_x": 88,
+        #         "SF_velocity_y": 138,
+        #     },
+        # ),
+        # # Test 5: velocity and scalar no traditional non-periodic
+        # (
+        #     np.array([[1, 2], [3, 4]]),
+        #     np.array([[1, -2], [-3, 4]]),
+        #     np.array([[3, -2], [-3, 12]]),
+        #     np.array([[-7, -18], [33, 52]]),
+        #     1,
+        #     1,
+        #     False,
+        #     np.array([[1, 2], [3, 4]]),
+        #     np.array([[3, -2], [-3, 12]]),
+        #     None,
+        #     None,
+        #     {
+        #         "SF_velocity_x": 88,
+        #         "SF_velocity_y": 138,
+        #         "SF_scalar_x": 5,
+        #         "SF_scalar_y": 8,
+        #     },
+        # ),
+        # # Test 6: all non-periodic
+        # (
+        #     np.array([[1, 2], [3, 4]]),
+        #     np.array([[1, -2], [-3, 4]]),
+        #     np.array([[3, -2], [-3, 12]]),
+        #     np.array([[-7, -18], [33, 52]]),
+        #     1,
+        #     1,
+        #     False,
+        #     np.array([[1, 2], [3, 4]]),
+        #     np.array([[3, -2], [-3, 12]]),
+        #     ["LL"],
+        #     None,
+        #     {
+        #         "SF_velocity_x": 88,
+        #         "SF_LL_x": 1,
+        #         "SF_scalar_x": 5,
+        #         "SF_velocity_y": 138,
+        #         "SF_LL_y": 26,
+        #         "SF_scalar_y": 8,
+        #     },
+        # ),
+        # # Test 7: all but skip velocity and no traditional non-periodic
+        # (
+        #     np.array([[1, 2], [3, 4]]),
+        #     np.array([[1, -2], [-3, 4]]),
+        #     np.array([[3, -2], [-3, 12]]),
+        #     np.array([[-7, -18], [33, 52]]),
+        #     1,
+        #     1,
+        #     True,
+        #     np.array([[1, 2], [3, 4]]),
+        #     np.array([[3, -2], [-3, 12]]),
+        #     None,
+        #     None,
+        #     {
+        #         "SF_scalar_x": 5,
+        #         "SF_scalar_y": 8,
+        #     },
+        # ),
+        # # Test 8: velocities with no scalar or traditional non-periodic
+        # (
+        #     np.array([[1, 2], [3, 4]]),
+        #     np.array([[1, -2], [-3, 4]]),
+        #     np.array([[3, -2], [-3, 12]]),
+        #     np.array([[-7, -18], [33, 52]]),
+        #     1,
+        #     1,
+        #     False,
+        #     None,
+        #     None,
+        #     None,
+        #     None,
+        #     {
+        #         "SF_velocity_x": 88,
+        #         "SF_velocity_y": 138,
+        #     },
+        # ),
+        # Test 9: linear velocities all SFs non-periodic
         (
-            np.array([[1, 2], [3, 4]]),  # u
-            np.array([[1, -2], [-3, 4]]),  # v
-            np.array([[3, -2], [-3, 12]]),  # adv_x
-            np.array([[-7, -18], [33, 52]]),  # adv_y
-            1,  # x_shift
-            1,  # y_shift
-            False,  # skip_velocity_sf
-            np.array([[1, 2], [3, 4]]),  # scalar
-            np.array([[3, -2], [-3, 12]]),  # adv_scalar
-            None,  # traditional_type
-            "periodic-all",  # boundary
+            np.meshgrid(np.arange(10), np.arange(10))[0], #u
+            0.5 * np.meshgrid(np.arange(10), np.arange(10))[0], #v
+            np.meshgrid(np.arange(10), np.arange(10))[0]
+            * np.gradient(
+                np.meshgrid(np.arange(10), np.arange(10))[0], 1, 1, axis=(1, 0)
+            )[0]
+            + 0.5
+            * np.meshgrid(np.arange(10), np.arange(10))[0]
+            * np.gradient(
+                np.meshgrid(np.arange(10), np.arange(10))[0], 1, 1, axis=(1, 0)
+            )[1], #adv_x
+            np.meshgrid(np.arange(10), np.arange(10))[0]
+            * np.gradient(
+                0.5 * np.meshgrid(np.arange(10), np.arange(10))[0], 1, 1, axis=(1, 0)
+            )[0]
+            + 0.5
+            * np.meshgrid(np.arange(10), np.arange(10))[0]
+            * np.gradient(
+                0.5 * np.meshgrid(np.arange(10), np.arange(10))[0], 1, 1, axis=(1, 0)
+            )[1], #adv_y
+            1, #shift_x
+            1, #shift_y
+            False, #skip_velocity_sf
+            None, #scalar
+            None, #adv_scalar
+            ["LL", "LLL", "LTT"], #traditional_type
+            None, #boundary
             {
-                "SF_velocity_x": 88,
-                "SF_velocity_y": 138,
-                "SF_scalar_x": 5,
-                "SF_scalar_y": 8,
-            },
-            # expected_result
-        ),
-        # Test 2: all
-        (
-            np.array([[1, 2], [3, 4]]),
-            np.array([[1, -2], [-3, 4]]),
-            np.array([[3, -2], [-3, 12]]),
-            np.array([[-7, -18], [33, 52]]),
-            1,
-            1,
-            False,
-            np.array([[1, 2], [3, 4]]),
-            np.array([[3, -2], [-3, 12]]),
-            ["LL"],
-            "periodic-all",
-            {
-                "SF_velocity_x": 88,
+                "SF_velocity_x": (5/4)*1,
                 "SF_LL_x": 1,
-                "SF_scalar_x": 5,
-                "SF_velocity_y": 138,
-                "SF_LL_y": 26,
-                "SF_scalar_y": 8,
+                "SF_LLL_x": 1,
+                "SF_LTT_x": (1/4)*1,
+                "SF_velocity_y": (5/4)*0,
+                "SF_LL_y": 0,
+                "SF_LLL_y": 0,
+                "SF_LTT_y": (1/4)*0,
             },
         ),
-        # Test 3: all but skip velocity and no traditional
+        # Test 10: linear velocities no traditional non-periodic
         (
-            np.array([[1, 2], [3, 4]]),
-            np.array([[1, -2], [-3, 4]]),
-            np.array([[3, -2], [-3, 12]]),
-            np.array([[-7, -18], [33, 52]]),
-            1,
-            1,
-            True,
-            np.array([[1, 2], [3, 4]]),
-            np.array([[3, -2], [-3, 12]]),
-            None,
-            "periodic-all",
+            np.meshgrid(np.arange(10), np.arange(10))[0], #u
+            0.5 * np.meshgrid(np.arange(10), np.arange(10))[0], #v
+            np.meshgrid(np.arange(10), np.arange(10))[0]
+            * np.gradient(
+                np.meshgrid(np.arange(10), np.arange(10))[0], 1, 1, axis=(1, 0)
+            )[0]
+            + 0.5
+            * np.meshgrid(np.arange(10), np.arange(10))[0]
+            * np.gradient(
+                np.meshgrid(np.arange(10), np.arange(10))[0], 1, 1, axis=(1, 0)
+            )[1], #adv_x
+            np.meshgrid(np.arange(10), np.arange(10))[0]
+            * np.gradient(
+                0.5 * np.meshgrid(np.arange(10), np.arange(10))[0], 1, 1, axis=(1, 0)
+            )[0]
+            + 0.5
+            * np.meshgrid(np.arange(10), np.arange(10))[0]
+            * np.gradient(
+                0.5 * np.meshgrid(np.arange(10), np.arange(10))[0], 1, 1, axis=(1, 0)
+            )[1], #adv_y
+            1, #shift_x
+            1, #shift_y
+            False, #skip_velocity_sf
+            None, #scalar
+            None, #adv_scalar
+            None, #traditional_type
+            None, #boundary
             {
-                "SF_scalar_x": 5,
-                "SF_scalar_y": 8,
+                "SF_velocity_x": (5/4)*1,
+                "SF_velocity_y": (5/4)*0,
             },
         ),
-        # Test 4: velocities with no scalar or traditional
+        # Test 11: linear velocities and scalar=v non-periodic
         (
-            np.array([[1, 2], [3, 4]]),
-            np.array([[1, -2], [-3, 4]]),
-            np.array([[3, -2], [-3, 12]]),
-            np.array([[-7, -18], [33, 52]]),
-            1,
-            1,
-            False,
-            None,
-            None,
-            None,
-            "periodic-all",
+            np.meshgrid(np.arange(10), np.arange(10))[0], #u
+            0.5 * np.meshgrid(np.arange(10), np.arange(10))[0], #v
+            np.meshgrid(np.arange(10), np.arange(10))[0]
+            * np.gradient(
+                np.meshgrid(np.arange(10), np.arange(10))[0], 1, 1, axis=(1, 0)
+            )[0]
+            + 0.5
+            * np.meshgrid(np.arange(10), np.arange(10))[0]
+            * np.gradient(
+                np.meshgrid(np.arange(10), np.arange(10))[0], 1, 1, axis=(1, 0)
+            )[1], #adv_x
+            np.meshgrid(np.arange(10), np.arange(10))[0]
+            * np.gradient(
+                0.5 * np.meshgrid(np.arange(10), np.arange(10))[0], 1, 1, axis=(1, 0)
+            )[0]
+            + 0.5
+            * np.meshgrid(np.arange(10), np.arange(10))[0]
+            * np.gradient(
+                0.5 * np.meshgrid(np.arange(10), np.arange(10))[0], 1, 1, axis=(1, 0)
+            )[1], #adv_y
+            1, #shift_x
+            1, #shift_y
+            False, #skip_velocity_sf
+            np.meshgrid(np.arange(10), np.arange(10))[0], #scalar
+            np.meshgrid(np.arange(10), np.arange(10))[0]
+            * np.gradient(
+                np.meshgrid(np.arange(10), np.arange(10))[0], 1, 1, axis=(1, 0)
+            )[0]
+            + 0.5
+            * np.meshgrid(np.arange(10), np.arange(10))[0]
+            * np.gradient(
+                np.meshgrid(np.arange(10), np.arange(10))[0], 1, 1, axis=(1, 0)
+            )[1], #adv_scalar
+            ["LL", "LLL", "LTT", "LSS"], #traditional_type
+            None, #boundary
             {
-                "SF_velocity_x": 88,
-                "SF_velocity_y": 138,
-            },
-        ),
-        # Test 5: velocity and scalar no traditional non-periodic
-        (
-            np.array([[1, 2], [3, 4]]),
-            np.array([[1, -2], [-3, 4]]),
-            np.array([[3, -2], [-3, 12]]),
-            np.array([[-7, -18], [33, 52]]),
-            1,
-            1,
-            False,
-            np.array([[1, 2], [3, 4]]),
-            np.array([[3, -2], [-3, 12]]),
-            None,
-            None,
-            {
-                "SF_velocity_x": 88,
-                "SF_velocity_y": 138,
-                "SF_scalar_x": 5,
-                "SF_scalar_y": 8,
-            },
-        ),
-        # Test 6: all non-periodic
-        (
-            np.array([[1, 2], [3, 4]]),
-            np.array([[1, -2], [-3, 4]]),
-            np.array([[3, -2], [-3, 12]]),
-            np.array([[-7, -18], [33, 52]]),
-            1,
-            1,
-            False,
-            np.array([[1, 2], [3, 4]]),
-            np.array([[3, -2], [-3, 12]]),
-            ["LL"],
-            None,
-            {
-                "SF_velocity_x": 88,
+                "SF_velocity_x": (5/4)*1,
                 "SF_LL_x": 1,
-                "SF_scalar_x": 5,
-                "SF_velocity_y": 138,
-                "SF_LL_y": 26,
-                "SF_scalar_y": 8,
-            },
-        ),
-        # Test 7: all but skip velocity and no traditional non-periodic
-        (
-            np.array([[1, 2], [3, 4]]),
-            np.array([[1, -2], [-3, 4]]),
-            np.array([[3, -2], [-3, 12]]),
-            np.array([[-7, -18], [33, 52]]),
-            1,
-            1,
-            True,
-            np.array([[1, 2], [3, 4]]),
-            np.array([[3, -2], [-3, 12]]),
-            None,
-            None,
-            {
-                "SF_scalar_x": 5,
-                "SF_scalar_y": 8,
-            },
-        ),
-        # Test 8: velocities with no scalar or traditional non-periodic
-        (
-            np.array([[1, 2], [3, 4]]),
-            np.array([[1, -2], [-3, 4]]),
-            np.array([[3, -2], [-3, 12]]),
-            np.array([[-7, -18], [33, 52]]),
-            1,
-            1,
-            False,
-            None,
-            None,
-            None,
-            None,
-            {
-                "SF_velocity_x": 88,
-                "SF_velocity_y": 138,
+                "SF_LLL_x": 1,
+                "SF_LTT_x": (1/4)*1,
+                "SF_velocity_y": (5/4)*0,
+                "SF_LL_y": 0,
+                "SF_LLL_y": 0,
+                "SF_LTT_y": (1/4)*0,
+                "SF_scalar_x": 1,
+                "SF_scalar_y": 0,
+                "SF_LSS_x": 1,
+                "SF_LSS_y": 0,
             },
         ),
     ],
