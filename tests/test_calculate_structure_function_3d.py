51e18cc2a026d7180676f208825f5d8b7cfcd1f1
Cassidy Wagner <cmwagner@protonmail.com>
2024-07-23 17:16:30 -0700

    wip test updates

--- tests/test_calculate_structure_function_3d.py
+++ tests/test_calculate_structure_function_3d.py
@@ -5,8 +5,8 @@ from fluidsf.calculate_structure_function_3d import calculate_structure_function
 
 
 @pytest.mark.parametrize(
-    "u, v, w, adv_x, adv_y, adv_z, shift_x, shift_y, shift_z, skip_velocity_sf, scalar,"
-    "adv_scalar, traditional_type, boundary, expected_result",
+    "u, v, w, adv_x, adv_y, adv_z, shift_x, shift_y, shift_z, sf_type, scalar,"
+    "adv_scalar, boundary, expected_result",
     [
         # Test 1: all with zero values
         (
@@ -19,29 +19,28 @@ from fluidsf.calculate_structure_function_3d import calculate_structure_function
             1,  # shift_x
             1,  # shift_y
             1,  # shift_z
-            False,  # skip_velocity_sf
+            ["ASF_V", "ASF_S", "LL", "LLL", "LTT", "LSS"],  # sf_type
             np.zeros((3, 3, 3)),  # scalar
             np.zeros((3, 3, 3)),  # adv_scalar
-            ["LL", "LLL", "LTT", "LSS"],  # traditional_type
             "periodic-all",  # boundary
             {
-                "SF_velocity_x": 0,
+                "SF_advection_velocity_x": 0,
                 "SF_LL_x": 0,
                 "SF_LLL_x": 0,
                 "SF_LTT_x": 0,
-                "SF_scalar_x": 0,
+                "SF_advection_scalar_x": 0,
                 "SF_LSS_x": 0,
-                "SF_velocity_y": 0,
+                "SF_advection_velocity_y": 0,
                 "SF_LL_y": 0,
                 "SF_LLL_y": 0,
                 "SF_LTT_y": 0,
-                "SF_scalar_y": 0,
+                "SF_advection_scalar_y": 0,
                 "SF_LSS_y": 0,
-                "SF_velocity_z": 0,
+                "SF_advection_velocity_z": 0,
                 "SF_LL_z": 0,
                 "SF_LLL_z": 0,
                 "SF_LTT_z": 0,
-                "SF_scalar_z": 0,
+                "SF_advection_scalar_z": 0,
                 "SF_LSS_z": 0,
             },
         ),
@@ -56,104 +55,131 @@ from fluidsf.calculate_structure_function_3d import calculate_structure_function
             1,  # shift_x
             1,  # shift_y
             1,  # shift_z
-            False,  # skip_velocity_sf
+            ["ASF_V", "ASF_S", "LL", "LLL", "LTT", "LSS"],  # sf_type
             np.ones((3, 3, 3)),  # scalar
             np.ones((3, 3, 3)),  # adv_scalar
-            ["LL", "LLL", "LTT", "LSS"],  # traditional_type
             "periodic-all",  # boundary
             {
-                "SF_velocity_x": 0,
+                "SF_advection_velocity_x": 0,
                 "SF_LL_x": 0,
                 "SF_LLL_x": 0,
                 "SF_LTT_x": 0,
-                "SF_scalar_x": 0,
+                "SF_advection_scalar_x": 0,
                 "SF_LSS_x": 0,
-                "SF_velocity_y": 0,
+                "SF_advection_velocity_y": 0,
                 "SF_LL_y": 0,
                 "SF_LLL_y": 0,
                 "SF_LTT_y": 0,
-                "SF_scalar_y": 0,
+                "SF_advection_scalar_y": 0,
                 "SF_LSS_y": 0,
-                "SF_velocity_z": 0,
+                "SF_advection_velocity_z": 0,
                 "SF_LL_z": 0,
                 "SF_LLL_z": 0,
                 "SF_LTT_z": 0,
-                "SF_scalar_z": 0,
+                "SF_advection_scalar_z": 0,
                 "SF_LSS_z": 0,
             },
         ),
-        # Test 3: all with non-zero values
+        # Test 3: linear velocities all SFs non-periodic no scalar
         (
-            np.arange(1, 28).reshape((3, 3, 3)),  # u
-            np.arange(1, 28).reshape((3, 3, 3)),  # v
-            np.arange(1, 28).reshape((3, 3, 3)),  # w
-            np.arange(1, 28).reshape((3, 3, 3)),  # adv_x
-            np.arange(1, 28).reshape((3, 3, 3)),  # adv_y
-            np.arange(1, 28).reshape((3, 3, 3)),  # adv_z
+            np.meshgrid(np.arange(10), np.arange(10), np.arange(10))[0],  # u
+            0.5 * np.meshgrid(np.arange(10), np.arange(10), np.arange(10))[1],  # v
+            np.meshgrid(np.arange(10), np.arange(10), np.arange(10))[2],  # w
+            np.meshgrid(np.arange(10), np.arange(10), np.arange(10))[0]
+            * np.gradient(
+                np.meshgrid(np.arange(10), np.arange(10), np.arange(10))[0],
+                1,
+                1,
+                1,
+                axis=(2, 1, 0),
+            )[0]
+            + 0.5
+            * np.meshgrid(np.arange(10), np.arange(10), np.arange(10))[1]
+            * np.gradient(
+                np.meshgrid(np.arange(10), np.arange(10), np.arange(10))[1],
+                1,
+                1,
+                1,
+                axis=(2, 1, 0),
+            )[1]
+            + np.meshgrid(np.arange(10), np.arange(10), np.arange(10))[2]
+            * np.gradient(
+                np.meshgrid(np.arange(10), np.arange(10), np.arange(10))[2],
+                1,
+                1,
+                1,
+                axis=(2, 1, 0),
+            )[2],  # adv_x
+            np.meshgrid(np.arange(10), np.arange(10), np.arange(10))[1]
+            * np.gradient(
+                0.5 * np.meshgrid(np.arange(10), np.arange(10), np.arange(10))[0],
+                1,
+                1,
+                1,
+                axis=(2, 1, 0),
+            )[0]
+            + 0.5
+            * np.meshgrid(np.arange(10), np.arange(10), np.arange(10))[1]
+            * np.gradient(
+                0.5 * np.meshgrid(np.arange(10), np.arange(10), np.arange(10))[1],
+                1,
+                1,
+                1,
+                axis=(2, 1, 0),
+            )[1]
+            + np.meshgrid(np.arange(10), np.arange(10), np.arange(10))[2]
+            * np.gradient(
+                0.5 * np.meshgrid(np.arange(10), np.arange(10), np.arange(10))[2],
+                1,
+                1,
+                1,
+                axis=(2, 1, 0),
+            )[2],  # adv_y
+            np.meshgrid(np.arange(10), np.arange(10), np.arange(10))[2]
+            * np.gradient(
+                np.meshgrid(np.arange(10), np.arange(10), np.arange(10))[0],
+                1,
+                1,
+                1,
+                axis=(2, 1, 0),
+            )[0]
+            + 0.5
+            * np.meshgrid(np.arange(10), np.arange(10), np.arange(10))[1]
+            * np.gradient(
+                np.meshgrid(np.arange(10), np.arange(10), np.arange(10))[1],
+                1,
+                1,
+                1,
+                axis=(2, 1, 0),
+            )[1]
+            + np.meshgrid(np.arange(10), np.arange(10), np.arange(10))[2]
+            * np.gradient(
+                np.meshgrid(np.arange(10), np.arange(10), np.arange(10))[2],
+                1,
+                1,
+                1,
+                axis=(2, 1, 0),
+            )[2],  # adv_z
             1,  # shift_x
             1,  # shift_y
             1,  # shift_z
-            False,  # skip_velocity_sf
-            np.arange(1, 28).reshape((3, 3, 3)),  # scalar
-            np.arange(1, 28).reshape((3, 3, 3)),  # adv_scalar
-            ["LL", "LLL", "LTT", "LSS"],  # traditional_type
+            ["ASF_V", "LL", "LLL", "LTT"],  # sf_type
+            None,  # scalar
+            None,  # adv_scalar
             None,  # boundary
             {
-                "SF_velocity_x": 12,
-                "SF_LL_x": 4,
-                "SF_LLL_x": 8,
-                "SF_LTT_x": 8,
-                "SF_scalar_x": 4,
-                "SF_LSS_x": 8,
-                "SF_velocity_y": 108,
-                "SF_LL_y": 36,
-                "SF_LLL_y": 216,
-                "SF_LTT_y": 216,
-                "SF_scalar_y": 36,
-                "SF_LSS_y": 216,
-                "SF_velocity_z": 972,
-                "SF_LL_z": 324,
-                "SF_LLL_z": 5832,
-                "SF_LTT_z": 5832,
-                "SF_scalar_z": 324,
-                "SF_LSS_z": 5832,
-            },
-        ),
-        # Test 4: velocities and advection with different values
-        (
-            np.arange(1, 28).reshape((3, 3, 3)),  # u
-            2 * np.arange(1, 28).reshape((3, 3, 3)),  # v
-            3 * np.arange(1, 28).reshape((3, 3, 3)),  # w
-            4 * np.arange(1, 28).reshape((3, 3, 3)),  # adv_x
-            5 * np.arange(1, 28).reshape((3, 3, 3)),  # adv_y
-            6 * np.arange(1, 28).reshape((3, 3, 3)),  # adv_z
-            1,  # shift_x
-            1,  # shift_y
-            1,  # shift_z
-            False,  # skip_velocity_sf
-            7 * np.arange(1, 28).reshape((3, 3, 3)),  # scalar
-            8 * np.arange(1, 28).reshape((3, 3, 3)),  # adv_scalar
-            ["LL", "LLL", "LTT", "LSS"],  # traditional_type
-            None,  # boundary
-            {
-                "SF_velocity_x": 128,
-                "SF_LL_x": 4,
-                "SF_LLL_x": 8,
-                "SF_LTT_x": 32,
-                "SF_scalar_x": 224,
-                "SF_LSS_x": 392,
-                "SF_velocity_y": 1152,
-                "SF_LL_y": 144,
-                "SF_LLL_y": 1728,
-                "SF_LTT_y": 432,
-                "SF_scalar_y": 2016,
-                "SF_LSS_y": 21168,
-                "SF_velocity_z": 10368,
-                "SF_LL_z": 2916,
-                "SF_LLL_z": 157464,
-                "SF_LTT_z": 17496,
-                "SF_scalar_z": 18144,
-                "SF_LSS_z": 857304,
+                "SF_advection_velocity_x": (5 / 4) * 1,
+                "SF_LL_x": 1,
+                "SF_LLL_x": 1,
+                "SF_LTT_x": (1 / 4) * 1,
+                "SF_advection_velocity_y": (5 / 4) * 0,
+                "SF_LL_y": 0,
+                "SF_LLL_y": 0,
+                "SF_LTT_y": (1 / 4) * 0,
+                "SF_advection_velocity_z": (5 / 4) * 0,
+                "SF_LL_z": 0,
+                "SF_LLL_z": 0,
+                "SF_LTT_z": (1 / 4) * 0,
             },
         ),
     ],
@@ -168,10 +194,9 @@ def test_calculate_structure_function_3d_parameterized(
     shift_x,
     shift_y,
     shift_z,
-    skip_velocity_sf,
+    sf_type,
     scalar,
     adv_scalar,
-    traditional_type,
     boundary,
     expected_result,
 ):
@@ -186,10 +211,9 @@ def test_calculate_structure_function_3d_parameterized(
         shift_x,
         shift_y,
         shift_z,
-        skip_velocity_sf,
+        sf_type,
         scalar,
         adv_scalar,
-        traditional_type,
         boundary,
     )
 
