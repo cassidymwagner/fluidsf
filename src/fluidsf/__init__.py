c284dc85d7592b999290bbb0c57597e817d2bcc2
Cassidy Wagner <cmwagner@protonmail.com>
2024-08-05 16:30:02 -0700

    allowing numpy 2 and pushing to test pypi again

--- src/fluidsf/__init__.py
+++ src/fluidsf/__init__.py
@@ -20,7 +20,7 @@ from .shift_array_1d import shift_array_1d
 from .shift_array_2d import shift_array_2d
 from .shift_array_3d import shift_array_3d
 
-__version__ = "0.1.1"
+__version__ = "0.1.2"
 
 __all__ = (
     "generate_sf_maps_2d",
