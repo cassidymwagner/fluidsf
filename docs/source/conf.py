634995aa16c050e538a6c1de8118f4d0a309c2bf
Cassidy Wagner <cmwagner@protonmail.com>
2024-08-06 22:04:04 -0700

    fixed autodoc bug with move to src

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -14,7 +14,7 @@ import sys
 
 sys.path.insert(0, os.path.abspath("."))
 sys.path.insert(0, os.path.abspath(".."))
-sys.path.insert(0, os.path.abspath("../.."))
+sys.path.insert(0, os.path.abspath("../../src"))
 
 # import mock
 
